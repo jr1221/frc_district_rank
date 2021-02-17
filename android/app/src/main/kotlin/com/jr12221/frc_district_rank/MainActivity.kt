4e009344986ea10ac9cf786f73aaaaf8e6059b88
Jack Rubacha <rubacha.jack03@gmail.com>
2021-02-17 16:47:21 -0500

    web build init

--- android/app/src/main/kotlin/com/jr12221/frc_district_rank/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.jr12221.frc_district_rank
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/ApiKey.dart
+++
@@ -1,4 +0,0 @@
-class ApiKey {
-  static const TBAKey =
-      'KMpingB75hZd8noCRQew4L8ZFEGikoSCGVfZx2x2i4BeL3pVs5C3L9llrEGIvuoB';
-}
